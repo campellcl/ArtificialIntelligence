a02978ab66a75aecff6b889167b0d9c41cea86ae
Chris Campell <ccampell.asu@gmail.com>
2017-09-20 14:23:02 -0400

    Implemented skeleton methods and added helper functions.

--- prog2/PetDetectiveProblem.py
+++ prog2/PetDetectiveProblem.py
@@ -3,6 +3,7 @@ PetDetectiveProblem.py
 Represents the PetDetective problem, subclasses aima's Problem class.
 """
 
+from copy import deepcopy
 from prog2.aima.search import Problem
 
 __author__ = "Chris Campell"
@@ -11,9 +12,39 @@ __version__ = "9/19/2017"
 
 class PetDetectiveProblem(Problem):
 
-    def __init__(self, initial_state, goal_state):
+    def __init__(self, initial_state, goal_state, game_board):
         super().__init__(initial=initial_state, goal=goal_state)
+        self.game_board = game_board
         # TODO: perform additional self.init as required.
+
+    def is_road(self, desired_location):
+        """
+        is_road: Returns if the desired location is a road ('-', '+', '|', {a-z,A-Z}).
+        :param desired_location: The location to check for existence of a road in the form (x,y).
+        :return boolean: True if the provided location is a road (may contain pet), false otherwise.
+        """
+        if self.game_board[desired_location[0]][desired_location[1]] != '.':
+            return True
+        else:
+            return False
+
+    def is_valid_action(self, agent_location, action):
+        """
+        is_valid_action: Determines if the specified action is executable based on the presence of the roadways and the
+            map edge.
+        :param action: The action that the agent wishes to perform in the context of the environment.
+        :return boolean: True if the action is executable according to game rules, false otherwise.
+        """
+        if action == 'u':
+            return self.is_road(desired_location=(agent_location[0] - 1, agent_location[1]))
+        elif action == 'r':
+            return self.is_road(desired_location=(agent_location[0], agent_location[1] + 1))
+        elif action == 'd':
+            return self.is_road(desired_location=(agent_location[0] + 1, agent_location[1]))
+        elif action == 'l':
+            return self.is_road(desired_location=(agent_location[0], agent_location[1] - 1))
+        else:
+            print("is_valid_action: Error, unknown agent action.")
 
     def actions(self, state):
         """
@@ -21,38 +52,68 @@ class PetDetectiveProblem(Problem):
         :param state: The state from which possible actions are to be determined.
         :return actions: A list of possible actions to be executed.
         """
-        # TODO: method body.
-        pass
+        actions = []
+        agent_loc = state['agent_loc']
+        if self.is_valid_action(agent_location=agent_loc, action='u'):
+            actions.append('u')
+        if self.is_valid_action(agent_location=agent_loc, action='r'):
+            actions.append('r')
+        if self.is_valid_action(agent_location=agent_loc, action='l'):
+            actions.append('l')
+        if self.is_valid_action(agent_location=agent_loc, action='d'):
+            actions.append('d')
+        return actions
 
     def result(self, state, action):
         """
-        result: Returns the state that results from executing the given action in the given state. The returned action
+        result: Returns the state that results from executing the given action in the given state. The action
             must be one of self.actions(state).
-        :param state:
-        :param action:
+        :param state: The initial state.
+        :param action: The action to apply to the initial state.
         :return resultant_state: The state that results from executing the given action in the provided state.
         """
-        # TODO: method body.
-        pass
+        resultant_state = deepcopy(state)
+        if action in self.actions(state=state):
+            # The action is valid and recognized.
+            if action == 'u':
+                updated_location = (state['agent_loc'][0] - 1, state['agent_loc'][1])
+            elif action == 'r':
+                updated_location = (state['agent_loc'][0], state['agent_loc'][1] + 1)
+            elif action == 'l':
+                updated_location = (state['agent_loc'][0], state['agent_loc'][1] - 1)
+            elif action == 'd':
+                updated_location = (state['agent_loc'][0] + 1, state['agent_loc'][1])
+            else:
+                print("This shouldn't happen.")
+            for pet, pet_location in state['pets_in_street'].items():
+                if updated_location == pet_location:
+                    # Append the pet to the car:
+                    resultant_state['pets_in_car'].append(pet)
+                    # Remove the pet from the street:
+                    resultant_state['pets_in_street'].pop(pet)
+            resultant_state['agent_loc'] = updated_location
+            return resultant_state
 
     def goal_test(self, state):
         """
         goal_test: Returns true if the state is a goal.
-        :param state:
+        :param state: The current state of the program represented by {'agent_loc': (x, y),
+            'pets_in_car': [], 'pets_in_street': {'pet': (x, y), ...}}
         :return is_goal: True if the provided state is a goal state, false otherwise.
         """
-        # TODO: method body.
-        pass
+        if len(state['pets_in_car']) == 0:
+            if not state['pets_in_street']:
+                return True
+        return False
 
     def path_cost(self, c, state1, action, state2):
         """
         path_cost: Returns the cost of a solution path that arrives at state2 from state1 via action, assuming cost 'c'
             to arrive at state1.
-        :param c:
-        :param state1:
-        :param action:
-        :param state2:
+        :param c: The cost of executing the given action (always 1 for pet detective).
+        :param state1: The initial state.
+        :param action: The action applied to the initial state which resulted in state2.
+        :param state2: The state reached as a result of applying the given action to state1.
         :return path_cost: The cost of a solution path that arrives at state2 from state1 assuming cost 'c'.
         """
-        # TODO: method body.
-        pass
+        return c
